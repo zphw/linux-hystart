496c1290a863244cae342e21d844865983d83cd2
zphw <zph@zph.io>
2022-06-20 22:42:13 -0400

    Updates

--- net/ipv4/tcp_cubic.c
+++ net/ipv4/tcp_cubic.c
@@ -434,7 +434,9 @@ static void hystart_update(struct sock *sk, u32 delay)
 			b0 = (tp->inet_conn.icsk_inet.inet_sport & 0x00ff) << 8u;
 			b1 = (tp->inet_conn.icsk_inet.inet_sport & 0xff00) >> 8u;
 			port = b0 | b1;
-			printk(KERN_INFO "CUBIC (port: %hu): Evaluating delay, Min RTT %d, Delay Exit Thresh %d\n", port, ca->delay_min, ca->delay_min + HYSTART_DELAY_THRESH(ca->delay_min >> 3));
+
+			printk(KERN_INFO "CUBIC (port: %hu): Evaluating delay, Current RTT %d, Delay Exit Thresh %d\n", port, ca->curr_rtt, ca->delay_min + HYSTART_DELAY_THRESH(ca->delay_min >> 3));
+
 			if (ca->curr_rtt > ca->delay_min +
 			    HYSTART_DELAY_THRESH(ca->delay_min >> 3)) {
 				printk(KERN_INFO "CUBIC (port: %hu): Exit due to delay detect\n", port);

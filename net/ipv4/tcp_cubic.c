ad7aab3f2671a84e6b1aef188a50d1b42d4ab8dc
zphw <zph@zph.io>
2022-07-07 15:52:01 -0400

    Updates

--- net/ipv4/tcp_cubic.c
+++ net/ipv4/tcp_cubic.c
@@ -422,7 +422,7 @@ static void hystart_update(struct sock *sk, u32 delay)
 
 			if ((now - ca->last_ack) > 100)
 			{
-				u64 curr_bytes_sent = (tp->bytes_sent - last_ack_bytes_sent) / 1000000;
+				u64 curr_bytes_sent = tp->bytes_sent - last_ack_bytes_sent;
 				u64 bitrate = (curr_bytes_sent * 8 * 1000000 / (now - ca->round_start)) / (1000 * 1000);
 				printk(KERN_INFO "CUBIC (port: %hu) [Round %hu] Now %u, Round Start %u, Since last ACK %u, Total Bytes Sent %lld, Current Bytes Send %lld, Bitrate %lld Mb/s\n",
 					port, round_id, now, ca->round_start, (now - ca->round_start), tp->bytes_sent / 1000000, curr_bytes_sent, bitrate);
